f93cc6d306babc8008a4fb8ed2c47f7f4737dfd9
Hung Nguyen <viethung1996@gmail.com>
2020-05-26 20:53:35 -0400

    feat: Complete exercise

--- course-04/exercises/lesson-2/starter-code/index.js
+++ course-04/exercises/lesson-2/starter-code/index.js
@@ -1,54 +1,91 @@
-'use strict'
+'use strict';
 
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
-const docClient = new AWS.DynamoDB.DocumentClient()
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-const groupsTable = process.env.GROUPS_TABLE
+const groupsTable = process.env.GROUPS_TABLE;
 
 exports.handler = async (event) => {
-  console.log('Processing event: ', event)
+  console.log('Processing event: ', event);
 
   // TODO: Read and parse "limit" and "nextKey" parameters from query parameters
-  // let nextKey // Next key to continue scan operation if necessary
-  // let limit // Maximum number of elements to return
+  let limit;
+  let nextKey;
 
+  // Maximum number of elements to return
+  // Next key to continue scan operation if necessary
   // HINT: You might find the following method useful to get an incoming parameter value
-  // getQueryParameter(event, 'param')
 
   // TODO: Return 400 error if parameters are invalid
+  try {
+    limit = parseLimitParam(event);
+    nextKey = parseNextKeyParam(event);
+  } catch (e) {
+    console.log('Failed to parse query params', e.message);
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        error: 'Invalid parameters',
+      }),
+    };
+  }
 
-  // Scan operation parameters
+  // Scan operation parameter
   const scanParams = {
     TableName: groupsTable,
     // TODO: Set correct pagination parameters
     // Limit: ???,
     // ExclusiveStartKey: ???
-  }
-  console.log('Scan params: ', scanParams)
+    Limit: limit,
+    ExclusiveStartKey: nextKey,
+  };
+  console.log('Scan params: ', scanParams);
 
-  const result = await docClient.scan(scanParams).promise()
+  const result = await docClient.scan(scanParams).promise();
 
-  const items = result.Items
+  const items = result.Items;
 
-  console.log('Result: ', result)
+  console.log('Result: ', result);
 
   // Return result
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
       items,
       // Encode the JSON object so a client can return it in a URL as is
-      nextKey: encodeNextKey(result.LastEvaluatedKey)
-    })
+      nextKey: encodeNextKey(result.LastEvaluatedKey),
+    }),
+  };
+};
+
+function parseLimitParam(event) {
+  let limit;
+  const limitStr = getQueryParameter(event, 'limit');
+  if (!limitStr) {
+    limit = 20;
+  } else {
+    limit = parseInt(limitStr, 10);
   }
+  if (limit < 0) throw new Error('Limit should be positive');
+  return limit;
+}
+
+function parseNextKeyParam(event) {
+  const nextKeyStr = getQueryParameter(event, 'nextKey');
+  if (!nextKeyStr) return null;
+  const uriDecoded = decodeURIComponent(nextKeyStr);
+  return JSON.parse(uriDecoded);
 }
 
 /**
- * Get a query parameter or return "undefined"
+ * Get a query paramet er or return "undefined"
  *
  * @param {Object} event HTTP event passed to a Lambda function
  * @param {string} name a name of a query parameter to return
@@ -56,12 +93,12 @@ exports.handler = async (event) => {
  * @returns {string} a value of a query parameter value or "undefined" if a parameter is not defined
  */
 function getQueryParameter(event, name) {
-  const queryParams = event.queryStringParameters
+  const queryParams = event.queryStringParameters;
   if (!queryParams) {
-    return undefined
+    return undefined;
   }
 
-  return queryParams[name]
+  return queryParams[name];
 }
 
 /**
@@ -73,8 +110,8 @@ function getQueryParameter(event, name) {
  */
 function encodeNextKey(lastEvaluatedKey) {
   if (!lastEvaluatedKey) {
-    return null
+    return null;
   }
 
-  return encodeURIComponent(JSON.stringify(lastEvaluatedKey))
+  return encodeURIComponent(JSON.stringify(lastEvaluatedKey));
 }
--- course-04/exercises/lesson-3/starter-code/src/lambda/http/getImages.ts
+++ course-04/exercises/lesson-3/starter-code/src/lambda/http/getImages.ts
@@ -1,66 +1,73 @@
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+} from 'aws-lambda';
+import 'source-map-support/register';
+import * as AWS from 'aws-sdk';
 
-const docClient = new AWS.DynamoDB.DocumentClient()
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-const groupsTable = process.env.GROUPS_TABLE
-const imagesTable = process.env.IMAGES_TABLE
+const groupsTable = process.env.GROUPS_TABLE;
+const imagesTable = process.env.IMAGES_TABLE;
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-
-  console.log('Caller event', event)
-  const groupId = event.pathParameters.groupId
-  const validGroupId = await groupExists(groupId)
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log('Caller event', event);
+  const groupId = event.pathParameters.groupId;
+  const validGroupId = await groupExists(groupId);
 
   if (!validGroupId) {
     return {
       statusCode: 404,
       headers: {
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
       },
       body: JSON.stringify({
-        error: 'Group does not exist'
-      })
-    }
+        error: 'Group does not exist',
+      }),
+    };
   }
 
-  const images = await getImagesPerGroup(groupId)
+  const images = await getImagesPerGroup(groupId);
 
   return {
     statusCode: 201,
     headers: {
-      'Access-Control-Allow-Origin': '*'
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
-      items: images
-    })
-  }
-}
+      items: images,
+    }),
+  };
+};
 
 async function groupExists(groupId: string) {
   const result = await docClient
     .get({
       TableName: groupsTable,
       Key: {
-        id: groupId
-      }
+        id: groupId,
+      },
     })
-    .promise()
+    .promise();
 
-  console.log('Get group: ', result)
-  return !!result.Item
+  console.log('Get group: ', result);
+  return !!result.Item; // convert to boolean
 }
 
 async function getImagesPerGroup(groupId: string) {
-  const result = await docClient.query({
-    TableName: imagesTable,
-    KeyConditionExpression: 'groupId = :groupId',
-    ExpressionAttributeValues: {
-      ':groupId': groupId
-    },
-    ScanIndexForward: false
-  }).promise()
+  const result = await docClient
+    .query({
+      TableName: imagesTable,
+      KeyConditionExpression: 'groupId = :groupId',
+      ExpressionAttributeValues: {
+        ':groupId': groupId,
+      },
+      ScanIndexForward: false,
+    })
+    .promise();
 
-  return result.Items
+  return result.Items;
 }
